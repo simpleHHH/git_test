d9564313f6901ca7fab743c9a5832bbe05706321
HHHHH <m18886019838@163.com>
2021-11-29 15:55:04 +0800

    hot-fix-commit1

--- src/main/java/hjx/GitTest.java
+++ src/main/java/hjx/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("git - idea");
         System.out.println("git - idea - 2");
         System.out.println("git - idea - 3");
+        System.out.println("hot-fix-1");
     }
 }
