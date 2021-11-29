78f2e2119c30058f7ce3228bfa0a049b0e473bca
HHHHH <m18886019838@163.com>
2021-11-29 15:59:08 +0800

    master-commit 2

--- src/main/java/hjx/GitTest.java
+++ src/main/java/hjx/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git - idea - 2");
         System.out.println("git - idea - 3");
         System.out.println("hot-fix-1");
+        System.out.println("hot-fix 和 master 冲突测试");
     }
 }
