b9ac8f436478f20cac1cd62e4d84dbbdc0286b4e
HHHHH <m18886019838@163.com>
2021-11-29 15:46:41 +0800

    idea_third_commit

--- src/main/java/hjx/GitTest.java
+++ src/main/java/hjx/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git - idea");
         System.out.println("git - idea - 2");
+        System.out.println("git - idea - 3");
     }
 }
